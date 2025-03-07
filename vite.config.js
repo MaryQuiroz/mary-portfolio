cbed6d221e24a1caf240e3eb45fec526e1956c18
Mary Quiroz <maryqr21@gmail.com>
2025-03-07 03:46:31 +0100

    fix: Configurar base URL para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/mary-portfolio/',
   // Eliminando la base URL para despliegues en Vercel
   // base: '/mary-portfolio/',
 })
